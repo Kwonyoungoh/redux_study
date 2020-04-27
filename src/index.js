b134cb4cf407c363eef4e7170b7b194cb03c2f88
Kwonyoungoh <57333646+Kwonyoungoh@users.noreply.github.com>
2020-04-27 15:01:46 +0900

    redux-vanila-counter

--- src/index.js
+++ src/index.js
@@ -6,22 +6,49 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-const countModifier = (count = 0, action) => {//default state를 initializing해주는 거임(초기화)
-  console.log(action);
-  // 액션을 통해 수정하는거임
+number.innerText = 0
 
-  if (action.type==="ADD"){
+//ACTIONS
+const ADD = "ADD";
+const MINUS = "MINUS";
+
+const countModifier = (count = 0, action) => {//default state를 initializing해주는 거임(초기화)
+switch (action.type) {
+  case ADD:
     return count + 1;
-  }
-  return count;
+  case MINUS:
+    return count - 1;
+  default:
+    return count;
+}
 };//data를 수정하는 함수다.
 //reducer든 modifier든 리턴하는 건 app의 count = state(data)
-
 const countStore = createStore(countModifier);
 
-countStore.dispatch({type:"ADD"});
+const handleAdd = () =>{
+  countStore.dispatch({type: ADD});
+}
 
-console.log(countStore.getState());
+const handleMin = () =>{
+  countStore.dispatch({type: MINUS});
+}
+
+const onChange = () => {
+  number.innerText = countStore.getState()
+}
+
+countStore.subscribe(onChange);
+
+add.addEventListener("click",handleAdd);
+minus.addEventListener("click",handleMin)
+
+
+// countStore.dispatch({type:"ADD"});//리듀서에게 액션을 보낸다.
+// countStore.dispatch({type:"ADD"});// 그러면 리듀서가 액션 타입에 맞게 data 즉 state를 바꿔준다 업데이트의 개념으로 알면안된다.
+// countStore.dispatch({type:"ADD"});
+// countStore.dispatch({type:"ADD"});
+
+// console.log(countStore.getState());
 
 // let count = 0;//이게 state임
 // number.innerText = count;
